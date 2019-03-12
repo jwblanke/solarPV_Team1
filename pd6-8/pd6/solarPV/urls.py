0a23eaa9b5d9d2586e8b02ab3db73bf57f790f0b
jwblanke <48179700+jwblanke@users.noreply.github.com>
2019-03-11 20:23:05 -0500

    Update

--- pd6-8/pd6/solarPV/urls.py
+++ pd6-8/pd6/solarPV/urls.py
@@ -4,5 +4,12 @@ from. import views
 
 urlpatterns = [
     path('solarPV/', views.solarPV, name='solarPV'),
-    path('registrationForm/', views.registrationForm, name='registrationForm')
+    path('RegistrationForm/', views.RegistrationForm, name='RegistrationForm'),
+    path('Certificate/', views.Certificate, name='Certificate'),
+    path('Client/', views.Client, name='Client'),
+    path('Location/', views.Location, name='Location'),
+    path('Map/', views.Map, name='Map'),
+    path('Product/', views.Product, name='Product'),
+    path('TestStandard/', views.TestStandard, name='TestStandard'),
+    path('Login/', views.Login, name='Login')
 ]
--- pd6-8/pd6/solarPV/views.py
+++ pd6-8/pd6/solarPV/views.py
@@ -5,5 +5,26 @@ from django.http import HttpResponse
 def solarPV(request):
     return render(request, 'solarPV/solarPV.html')
 
-def registrationForm(request):
-    return render(request, 'solarPV/registrationForm.html')
+def RegistrationForm(request):
+    return render(request, 'solarPV/RegistrationForm.html')
+
+def Certificate(request):
+    return render(request, 'solarPV/Certificate.html')
+
+def Client(request):
+    return render(request, 'solarPV/Client.html')
+
+def Location(request):
+    return render(request, 'solarPV/Location.html')
+
+def Map(request):
+    return render(request, 'solarPV/Map.html')
+
+def Product(request):
+    return render(request, 'solarPV/Product.html')
+
+def TestStandard(request):
+    return render(request, 'solarPV/TestStandard.html')
+
+def Login(request):
+    return render(request, 'solarPV/Login.html')
