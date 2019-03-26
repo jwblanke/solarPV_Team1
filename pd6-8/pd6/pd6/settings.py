388dac225a119ebdf59c7da4c48fae72a693f545
fionabronwen <fionabronwen@gmail.com>
2019-03-25 21:07:15 -0500

    pd7 - create backend app, modules, database connection

--- pd6-8/pd6/pd6/settings.py
+++ pd6-8/pd6/pd6/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'backend',
     'solarPV',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'pd6.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SolarPV',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '2020'
     }
 }
 
