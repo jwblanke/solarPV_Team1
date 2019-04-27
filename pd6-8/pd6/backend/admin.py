5e651172d1ccfd8f9825de4490919ef4ea3c6a0e
fionabronwen <fionabronwen@gmail.com>
2019-04-26 23:52:00 -0500

    Implement login

--- pd6-8/pd6/backend/admin.py
+++ pd6-8/pd6/backend/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Product, Certificate, Client, Location, TestStandard
-
+import os
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Certificate)
@@ -8,5 +8,8 @@ admin.site.register(Client)
 admin.site.register(Location)
 admin.site.register(TestStandard)
 
+dirname = os.path.dirname(__file__)
+login_template = os.path.join(dirname, '../solarPV/templates/registration/login.html')
+admin.site.login_template = login_template
 admin.site.site_header = 'SolarPV Dashboard'
 admin.site.site_url = '/solarPV'
